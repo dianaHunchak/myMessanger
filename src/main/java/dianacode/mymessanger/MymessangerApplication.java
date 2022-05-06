5cebed45825cb90dd6ebcc3116c55dc30c5a957b
Diana Hunchak <hunchak.diana.v@gmail.com>
2022-05-06 14:28:06 +0200

    commit

---
+++ src/main/java/dianacode/mymessanger/MymessangerApplication.java
@@ -0,0 +1,13 @@
+package dianacode.mymessanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymessangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymessangerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dianacode/mymessanger/MymessangerApplicationTests.java
@@ -0,0 +1,13 @@
+package dianacode.mymessanger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymessangerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
